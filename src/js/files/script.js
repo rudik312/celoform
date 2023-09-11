4443d7ebc291adb375b79c17123f80eed45f8f40
rudik312 <fl.rudik@mail.ru>
2023-09-11 20:24:45 +0700

    update script.js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -10,9 +10,9 @@ window.onload = function() {
 //Работаем с табами
 const tabs = document.querySelectorAll(".tabheader__item"),
 tabsContent = document.querySelectorAll(".tabcontent"),
-tabsParent = document.querySelector(".tabheader__size"),
+tabsParent = document.querySelector(".tabheader__size");
 
-prices = document.querySelectorAll('.tabheader__price-item');
+// prices = document.querySelectorAll('.tabheader__price-item');
 
 function hideTabContent() {
 tabsContent.forEach((item) => {
@@ -24,30 +24,49 @@ tabs.forEach((item) => {
   item.classList.remove("tabheader__item_active");
 });
 
-prices.forEach((item) =>  {
-  item.classList.add("hide");
-  item.classList.remove("show", "fade");
-});
+// prices.forEach((item) =>  {
+//   item.classList.add("hide");
+//   item.classList.remove("show", "fade");
+// });
 
 
 }
+
 function showTabContent(i = 1) {
-tabsContent[i].classList.add("show", "fades");
-tabsContent[i].classList.remove("hide");
-tabs[i].classList.add("tabheader__item_active");
+
+  if (typeof tabsContent !== 'undefined' && tabsContent.length > 0) {
+
+    // Тут код, который обрабатывает объект
+    tabsContent[i].classList.add("show", "fades");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+
 }
 
-function showTabPrice(i = 1) {
-prices[i].classList.add("show", "fades");
-prices[i].classList.remove("hide");
-}
+// function showTabPrice(i = 1) {
+// prices[i].classList.add("show", "fades");
+// prices[i].classList.remove("hide");
+// }
 
 hideTabContent();
 showTabContent();
-showTabPrice();
+// showTabPrice();
 
 
-tabsParent.addEventListener("click", function(event) {
+const setListener = (element, type, handler) => {
+  if(!element) {
+    return; 
+  }
+  element.addEventListener(type, handler);
+  return () => {
+    element.removeEventListener(type, handler);
+  };
+}
+
+
+setListener(tabsParent, "click", function(event) {
 const target = event.target;
 // console.dir(target);
 
@@ -58,7 +77,7 @@ if (target && target.classList.contains("tabheader__item")) {
     if (target == item) {
       hideTabContent();
       showTabContent(i);
-      showTabPrice(i);
+      // showTabPrice(i);
     }
   });
 }
@@ -67,13 +86,26 @@ if (target && target.classList.contains("tabheader__item")) {
 
 
   // функция работы MixitUp
-  $(function () {
-    var mixer = mixitup('.tabheader__box', {
-      animation: {
-        duration: 400,
-        effects: 'fade opacity(1)'
+  // $(function () {
+  //   var mixer = mixitup('.tabheader__box', {
+  //     animation: {
+  //       duration: 400,
+  //       effects: 'fade opacity(1)'
+  //   }
+  //   });
+  // });
+
+
+
+  var containerEl = document.querySelector('.tabheader__box');
+    var mixer;
+    if (containerEl) {
+      mixer = mixitup(containerEl, {
+        animation: {
+          duration: 400,
+          effects: 'fade opacity(1)'
+      }
+      });
     }
-    });
-  });
 
 }
